74e78547d12e29cbd27fac780ae7ed6bc567ef6a
bakbaloch <bakhtawerbashirbaloch@gmail.com>
2025-03-30 23:29:48 -0400

    Descriptive commit message about changes made

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+
 import Hero from './components/Hero'
 import Services from './components/Services'
 import Download from './components/Download'
 import HowItWorks from './components/HowItWorks';
 import Footer from './components/Footer';
-import Navbar from './components/Navbar'
 
 
 
